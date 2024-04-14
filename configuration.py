f48f3a83e557a35157be78149585758247a0f5de
Anayalab <hannabananal@yandex.ru>
2024-04-14 22:48:11 +0300

    Fix constant variable name

--- configuration.py
+++ configuration.py
@@ -1,5 +1,5 @@
 # Лабазина Анна, 15-я когорта — Дипломный проект
 
 URL_SERVICE = "https://6fa840ea-7245-48ba-9386-806b814df022.serverhub.praktikum-services.ru"
-CREAT_ORDERS = "/api/v1/orders"
+CREATE_ORDERS = "/api/v1/orders"
 TRACK_ORDERS = "/api/v1/orders/track"
